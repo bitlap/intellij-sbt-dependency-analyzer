6e4ebe699963699e66b642613d90bcb8ddb75897
梦境迷离 <dreamylost@outlook.com>
2023-08-17 23:09:40 +0800

    tmp

--- src/main/scala/bitlap/sbt/analyzer/SbtShellTask.scala
+++ src/main/scala/bitlap/sbt/analyzer/SbtShellTask.scala
@@ -16,46 +16,71 @@ import com.intellij.openapi.project.Project
  */
 trait SbtShellTask[T] {
 
+  final def getCommandOutputLines(project: Project, command: String): List[String] = {
+    val comms       = SbtShellCommunication.forProject(project)
+    val outputLines = ListBuffer[String]()
+    val executed = comms.command(
+      command,
+      new StringBuilder(),
+      SbtShellCommunication.listenerAggregator {
+        case SbtShellCommunication.Output(line) =>
+          outputLines.append(line.trim)
+        case _ =>
+      }
+    )
+    Await.result(executed, 5.minutes)
+    outputLines.toList.filter(_.startsWith("[info]"))
+  }
+
   def executeTask(project: Project): T
 
 }
 
 object SbtShellTask {
 
-  final val shellOutputResultRegex = "(\\[info\\])(\\s|\\t)*(.*)".r
-  final val cmdNameRegex           = "(\\[info\\])(\\s|\\t)*(.*)(\\s|\\t)*/(\\s|\\t)*moduleName".r
-  final val rootCmdNameRegex       = "(\\[info\\])(\\s|\\t)*moduleName".r
+  final case class LibraryModuleID(
+    organization: String,
+    name: String,
+    revision: String,
+    configurations: Option[String] = None, // not used
+    isChanging: Boolean = false,           // not used
+    isTransitive: Boolean = false,         // not used
+    isForce: Boolean = false               // not used
+  )
+
+  // moduleName
+  final val shellOutputResultRegex   = "(\\[info\\])(\\s|\\t)*(.*)".r
+  final val moduleNameInputRegex     = "(\\[info\\])(\\s|\\t)*(.*)(\\s|\\t)*/(\\s|\\t)*moduleName".r
+  final val rootModuleNameInputRegex = "(\\[info\\])(\\s|\\t)*moduleName".r
+
+  // libraryDependencies
+  final val libraryDependenciesInputRegex     = "(\\[info\\])(\\s|\\t)*(.*)(\\s|\\t)*/(\\s|\\t)*libraryDependencies".r
+  final val rootLibraryDependenciesInputRegex = "(\\[info\\])(\\s|\\t)*libraryDependencies".r
+  final val libraryDependenciesOutputRegex    = "List\\((.*)\\)".r
+  final val shellOutputStarResultRegex        = "(\\[info\\])(\\s|\\t)*(\\*)(\\s|\\t)*(.*)".r
+  // libraryDependencies ModuleID
+  final val libraryDependenciesOutput1 = "(.*):(.*):(.*)".r
+  final val libraryDependenciesOutput2 = "(.*):(.*):(.*):(.*)".r
+  final val libraryDependenciesOutput3 = "(.*):(.*):(.*):(.*);(.*)".r
+  final val libraryDependenciesOutput4 = "(.*):(.*):(.*):(.*)(\\s|\\t)*(.*)".r
 
   lazy val sbtModuleNamesTask: SbtShellTask[Map[String, String]] = new SbtShellTask[Map[String, String]]:
 
     override def executeTask(project: Project): Map[String, String] = {
-      val comms                    = SbtShellCommunication.forProject(project)
+      val mms                      = getCommandOutputLines(project, "moduleName")
       val moduleIdSbtModuleNameMap = scala.collection.mutable.HashMap[String, String]()
-      val outputLines              = ListBuffer[String]()
-      val executed = comms.command(
-        "moduleName",
-        new StringBuilder(),
-        SbtShellCommunication.listenerAggregator {
-          case SbtShellCommunication.Output(line) =>
-            outputLines.append(line.trim)
-          case _ =>
-        }
-      )
-      Await.result(executed, 5.minutes)
-
-      val mms = outputLines.filter(_.startsWith("[info]"))
       if (mms.size % 2 == 0) {
-        for (i <- 0 until mms.size - 1 by (2)) {
+        for (i <- 0 until mms.size - 1 by 2) {
           moduleIdSbtModuleNameMap.put(mms(i).trim, mms(i + 1).trim)
         }
       } else if (mms.size == 1) moduleIdSbtModuleNameMap.put(SingleSbtModule, mms(0))
 
       moduleIdSbtModuleNameMap.map { (k, v) =>
         val key = k match
-          case cmdNameRegex(_, _, moduleName, _, _) => moduleName.trim
-          case rootCmdNameRegex(_, _)               => RootSbtModule
-          case SingleSbtModule                      => SingleSbtModule
-          case _                                    => Constants.Empty_String
+          case moduleNameInputRegex(_, _, moduleName, _, _) => moduleName.trim
+          case rootModuleNameInputRegex(_, _)               => RootSbtModule
+          case SingleSbtModule                              => SingleSbtModule
+          case _                                            => Constants.Empty_String
 
         val value = v match
           case shellOutputResultRegex(_, _, sbtModuleName) => sbtModuleName.trim
@@ -69,21 +94,71 @@ object SbtShellTask {
   lazy val organizationTask: SbtShellTask[String] = new SbtShellTask[String]:
 
     override def executeTask(project: Project): String = {
-      val comms       = SbtShellCommunication.forProject(project)
-      val outputLines = ListBuffer[String]()
-      val executed = comms.command(
-        "organization",
-        new StringBuilder(),
-        SbtShellCommunication.listenerAggregator {
-          case SbtShellCommunication.Output(line) =>
-            outputLines.append(line.trim)
-          case _ =>
-        }
-      )
-      Await.result(executed, 5.minutes)
-      outputLines.filter(_.startsWith("[info]")).lastOption.getOrElse("") match
+      val outputLines = getCommandOutputLines(project, "organization")
+      outputLines.lastOption.getOrElse("") match
         case shellOutputResultRegex(_, _, org) =>
           org.trim
         case _ => null
+    }
+
+  lazy val libraryDependenciesTask: SbtShellTask[Map[String, List[LibraryModuleID]]] = new SbtShellTask[Map[String, List[LibraryModuleID]]]:
+
+    private def moduleIDArtifact(output: String): Option[LibraryModuleID] = {
+      output.trim match
+        case libraryDependenciesOutput4(group, artifact, revision, _, _) =>
+          Some(LibraryModuleID(group.trim, artifact.trim, revision.trim))
+        case libraryDependenciesOutput3(group, artifact, revision, conf, _, _) =>
+          Some(LibraryModuleID(group.trim, artifact.trim, revision.trim))  
+        case libraryDependenciesOutput2(group, artifact, revision, conf) =>
+          Some(LibraryModuleID(group.trim, artifact.trim, revision.trim))  
+        case libraryDependenciesOutput1(group, artifact, revision) =>
+          Some(LibraryModuleID(group.trim, artifact.trim, revision.trim))  
+        case _ =>
+          None
+    }
+
+    override def executeTask(project: Project): Map[String, List[LibraryModuleID]] = {
+      val outputLines              = getCommandOutputLines(project, "libraryDependencies")
+      val moduleIdSbtLibrariesMap = scala.collection.mutable.HashMap[String, String]()
+      // single module
+      if (outputLines.startsWith("[info] *")) {
+        val libraries = outputLines.map {
+          case shellOutputStarResultRegex(_, _, _, _, artifact) =>
+            moduleIDArtifact(artifact)
+          case _ => None
+        }.collect { case Some(value) =>
+          value
+        }
+        return Map(SingleSbtModule -> libraries)
+
+      }
+      // multi-module
+      if (outputLines.size % 2 == 0) {
+        for (i <- 0 until outputLines.size - 1 by 2) {
+          moduleIdSbtLibrariesMap.put(outputLines(i).trim, outputLines(i + 1).trim)
+        }
+      }
+
+      moduleIdSbtLibrariesMap.map { (k, v) =>
+        val key = k match
+          case libraryDependenciesInputRegex(_, _, moduleName, _, _) => moduleName.trim
+          case rootLibraryDependenciesInputRegex(_, _)               => RootSbtModule
+          case _                                                     => Constants.Empty_String
+
+        val value = v match
+          case shellOutputResultRegex(_, _, output) =>
+            output.trim match
+              case libraryDependenciesOutputRegex(modules) =>
+                modules.split(",").toList.map(_.trim).map(moduleIDArtifact).collect { case Some(value) =>
+                  value
+                }
+              case _ => List.empty
+          case _ => List.empty
+
+        key -> value
+      }.collect {
+        case (k, v) if k != Constants.Empty_String => k -> v
+      }.toMap
+
     }
 }
--- src/test/scala/bitlap/sbt/analyzer/SbtShellTaskRegex.scala
+++ src/test/scala/bitlap/sbt/analyzer/SbtShellTaskRegex.scala
@@ -15,13 +15,61 @@ class SbtShellTaskRegex extends AnyFlatSpec {
       case _ => assert(false)
 
     "[info] rolls-csv / moduleName" match
-      case SbtShellTask.cmdNameRegex(_, _, moduleName, _, _) =>
+      case SbtShellTask.moduleNameInputRegex(_, _, moduleName, _, _) =>
         assert(moduleName.trim == "rolls-csv")
       case _ => assert(false)
 
     "[info] moduleName" match
-      case SbtShellTask.rootCmdNameRegex(_, _) =>
+      case SbtShellTask.rootModuleNameInputRegex(_, _) =>
         assert(true)
+      case _ => assert(false)
+
+    "[info] discovery / libraryDependencies" match
+      case SbtShellTask.libraryDependenciesInputRegex(_, _, module, _, _) =>
+        assert(module.trim == "discovery")
+      case _ => assert(false)
+
+    "[info] libraryDependencies" match
+      case SbtShellTask.rootLibraryDependenciesInputRegex(_, _) =>
+        assert(true)
+      case _ => assert(false)
+
+    "List(org.scala-lang:scala-library:2.13.11, junit:junit:4.13.2:test, org.scalatest:scalatest:3.2.14:test)" match
+      case SbtShellTask.libraryDependenciesOutputRegex(modules) =>
+        assert(
+          modules == "org.scala-lang:scala-library:2.13.11, junit:junit:4.13.2:test, org.scalatest:scalatest:3.2.14:test"
+        )
+      case _ => assert(false)
+
+    "[info] * org.scalatest:scalatest:3.2.16:test" match
+      case SbtShellTask.shellOutputStarResultRegex(_, _, _, _, artifact) =>
+        assert(artifact == "org.scalatest:scalatest:3.2.16:test")
+      case _ => assert(false)
+
+    "org.scalatest:scalatest:3.2.16" match
+      case SbtShellTask.libraryDependenciesOutput1(group, _, _) =>
+        assert(group == "org.scalatest")
+      case _ => assert(false)
+
+    "org.scalatest:scalatest:3.2.16:optional" match
+      case SbtShellTask.libraryDependenciesOutput2(group, _, _, _) =>
+        assert(group == "org.scalatest")
+      case _ => assert(false)
+
+    "org.scalatest:scalatest:3.2.16:test" match
+      case SbtShellTask.libraryDependenciesOutput2(group, _, _, _) =>
+        assert(group == "org.scalatest")
+      case _ => assert(false)
+
+    "org.scalatest:scalatest:3.2.16:test force" match
+      case SbtShellTask.libraryDependenciesOutput4(group, _, _, _, _, _) =>
+        assert(group == "org.scalatest")
+      case _ => assert(false)
+
+    "org.scalatest:scalatest:3.2.16:optional;provided" match
+      case SbtShellTask.libraryDependenciesOutput3(group, _, _, opt, _) =>
+        assert(group == "org.scalatest")
+        assert(opt == "optional")
       case _ => assert(false)
   }
 
